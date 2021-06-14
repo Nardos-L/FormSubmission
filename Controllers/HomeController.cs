6732b30bb3fa9310a1b3590a4a683157973bebc3
Nardos Lengo <nardi.8199@gmail.com>
2021-06-14 12:55:37 -0700

    sucess page added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,13 +24,20 @@ namespace FormSubmission.Controllers
             if (ModelState.IsValid)
             {
                 // do somethng!  maybe insert into db?  then we will redirect
-                return RedirectToAction("Create");
+                return RedirectToAction("Success");
             }
             else
             {
                 // Oh no!  We need to return a ViewResponse to preserve the ModelState, and the errors it now contains!
                 return View("Index");
             }
+        }
+
+        [HttpGet("sucess")]
+
+        public IActionResult Success()
+        {
+            return View("Create");
         }
 
 
